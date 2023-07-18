0bc60fc0acba8d451568e1d4dbc14c66eef69561
Karen Nikoghosyan <karenn@synopsys.com>
2023-07-18 21:40:34 +0400

    Added releative paths

--- AudioSplittingBySentance/Config.py
+++ AudioSplittingBySentance/Config.py
@@ -1,38 +1,29 @@
 import os
 
-docx_file_path = "C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\002.docx"
+docx_file_path = "input\\002.docx"
 
-audio_file_path = 'C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\002.mp3'
-wav_file_path = "C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\002.wav"
+audio_file_path = 'input\\002.mp3'
+wav_file_path = "input\\002.wav"
 
-output_dir_path = 'C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\output\\'
+output_dir_path = 'output\\'
 if not os.path.exists(output_dir_path):
     os.makedirs(output_dir_path)
     print("Created folder:", output_dir_path)
 else:
     print(output_dir_path, "folder already exists.")
 
-experiments_output_dir_path = 'C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\output\\experiments\\'
-if not os.path.exists(experiments_output_dir_path):
-    os.makedirs(experiments_output_dir_path)
-    print("Created folder:", experiments_output_dir_path)
-else:
-    print(experiments_output_dir_path, "folder already exists.")
-
-adamamutin_file_path_mp3 = "C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\adamamutin_anahit_kirakosyan\\001_cut.mp3"
-adamamutin_file_path_wav = "C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\adamamutin_anahit_kirakosyan\\001_cut.wav"
-adamamutin_docx_file_path = "C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\adamamutin_anahit_kirakosyan\\adamamutin.docx"
-adamamutin_output_dir_path = 'C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\output\\adamamutin_anahit_kirakosyan\\wavs\\'
+adamamutin_file_path_mp3 = "input\\adamamutin_anahit_kirakosyan\\001_cut.mp3"
+adamamutin_file_path_wav = "input\\adamamutin_anahit_kirakosyan\\001_cut.wav"
+adamamutin_docx_file_path = "input\\adamamutin_anahit_kirakosyan\\adamamutin.docx"
+adamamutin_output_dir_path = 'output\\adamamutin_anahit_kirakosyan\\wavs\\'
 if not os.path.exists(adamamutin_output_dir_path):
     os.makedirs(adamamutin_output_dir_path)
     print("Created folder:", adamamutin_output_dir_path)
 else:
     print(adamamutin_output_dir_path, "folder already exists.")
 
-adamamutin_output_csv_dir_path = 'C:\\Users\\karen\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\output\\adamamutin_anahit_kirakosyan\\metadata.csv'
+adamamutin_output_csv_dir_path = 'output\\adamamutin_anahit_kirakosyan\\metadata.csv'
 
 
 min_diff_threshold = 0.7
 split_character=':'
-
-pauses_symbols=['—',',',':','.','...']
