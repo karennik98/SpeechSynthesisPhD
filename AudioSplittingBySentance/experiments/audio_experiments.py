158517249a1577e8cca0baf7feafb3e45bbddabb
Karen Nikoghosyan <karen.nikoghosyan.98@gmail.com>
2023-11-26 02:24:28 +0400

    stere to mono conversation

--- AudioSplittingBySentance/experiments/audio_experiments.py
+++ AudioSplittingBySentance/experiments/audio_experiments.py
@@ -1,10 +1,23 @@
 import AudioUtilities
 from pydub.utils import mediainfo
 
-if __name__ == '__main__':
-    info = AudioUtilities.get_audio_info("C:\\Users\\karenn\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\hopopy\\001.wav")
-    for key, value in info.items():
-        print(f"{key}: {value}")
+from pydub import AudioSegment
 
-    info2 = mediainfo("C:\\Users\\karenn\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\hopopy\\001.wav")
-    print(info2)
+def stereo_to_mono(input_path, output_path):
+    # Load the stereo audio file
+    audio = AudioSegment.from_file(input_path)
+
+    # Convert to mono by averaging left and right channels
+    mono_audio = audio.set_channels(1)
+
+    # Export the mono audio to a new file
+    mono_audio.export(output_path, format="wav")
+
+if __name__ == '__main__':
+    # Replace 'input_stereo.wav' and 'output_mono.wav' with your file paths
+    input_file_path = 'C:\\Users\\karenn\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\hopopy\\001.wav'
+    output_file_path = 'C:\\Users\\karenn\\PhD\\SpeechSynthesisPhD\\AudioSplittingBySentance\\input\\hopopy\\001_mono.wav'
+
+    stereo_to_mono(input_file_path, output_file_path)
+    info = mediainfo(output_file_path)
+    print(info)
